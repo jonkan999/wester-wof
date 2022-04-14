8bde486b0ea463a33c9df19100212509360c66e6
jonkan999 <83243590+jonkan999@users.noreply.github.com>
2022-04-14 10:06:36 +0200

    Add files via upload

--- src/Winning.js
+++ src/Winning.js
@@ -4,6 +4,7 @@ import callAPI from './callAPI.js';
 import Confetti from 'react-confetti'
 import './Wheel.css';
 
+let westers_url='https://www.westers.se/?gclid=CjwKCAjw6dmSBhBkEiwA_W-EoMIZ_n1hUj9x3TPdAkUCPblovF-hdikZhxGPho7RezLj2P_OKW6-WBoC31gQAvD_BwE'
 
 export default class Winning extends React.Component {
     
@@ -12,7 +13,7 @@ export default class Winning extends React.Component {
         callAPI(this.props.values.business,this.props.values.email,"nowin")
         return (
             <div className="winner-text">
-                Tyvärr! Ingen vinst idag, men gå gärna ändå in och kika på vad vi skulle kunna erbjuda er i form av Catering och Event på: <a style={{ color: "hotpink"}} href='https://www.westers.se/?gclid=CjwKCAjw6dmSBhBkEiwA_W-EoMIZ_n1hUj9x3TPdAkUCPblovF-hdikZhxGPho7RezLj2P_OKW6-WBoC31gQAvD_BwE' target="_blank" title="Opens in a new window">www.westers.se</a>
+                Tyvärr! Ingen vinst idag, men gå gärna ändå in och kika på vad vi skulle kunna erbjuda er i form av Catering och Event på: <a style={{ color: "hotpink"}} href={westers_url} target="_blank" title="Opens in a new window">www.westers.se</a>
                 <Confetti
                     width={window.innerWidth}
                     height={window.innerHeight}
@@ -24,12 +25,7 @@ export default class Winning extends React.Component {
         callAPI(this.props.values.business,this.props.values.email,"Kock")
         return (
             <div className="winner-text">
-                Grattis! Wow! Du är nu med i utlottningen till att vinna en personlig kock i hemmet. Vi kommer och serverar och tillagar en tre rätters meny, i ditt hem! Vi hör av oss på mail om du vinner, så länge kan du läsa mer om det och våra andra tjänster för Catering och Event på: <a style={{ color: "hotpink"}} href='https://www.westers.se/?gclid=CjwKCAjw6dmSBhBkEiwA_W-EoMIZ_n1hUj9x3TPdAkUCPblovF-hdikZhxGPho7RezLj2P_OKW6-WBoC31gQAvD_BwE' target="_blank" title="Opens in a new window">www.westers.se</a>
-                <Confetti
-                    width={window.innerWidth}
-                    height={window.innerHeight}
-                    colors={['#B5BD02','#ED8B00','#CE0037']}
-                    />
+                Grattis! Wow! Du är nu med i utlottningen till att vinna en personlig kock i hemmet. Vi kommer och serverar och tillagar en tre rätters meny, i ditt hem! Vi hör av oss på mail om du vinner, så länge kan du läsa mer om det och våra andra tjänster för Catering och Event på: <a style={{ color: "hotpink"}} href={westers_url} target="_blank" title="Opens in a new window">www.westers.se</a>
             </div>
             );
 
@@ -37,7 +33,7 @@ export default class Winning extends React.Component {
         callAPI(this.props.values.business,this.props.values.email,"SoL")
         return (
             <div className="winner-text">
-                 Grattis! Wow! Du är nu med i utlottningen till att vinna en trerätters meny för 4 personer på Westers sommarkrog Sjö & Land. Vi kommer och serverar och tillagar en tre rätters meny, i ditt hem! Vi hör av oss på mail om du vinner, så länge kan du läsa mer om det och våra andra tjänster för Catering och Event på: <a style={{ color: "hotpink"}} href='https://www.westers.se/?gclid=CjwKCAjw6dmSBhBkEiwA_W-EoMIZ_n1hUj9x3TPdAkUCPblovF-hdikZhxGPho7RezLj2P_OKW6-WBoC31gQAvD_BwE' target="_blank" title="Opens in a new window">www.westers.se</a> och på restaurangen: <a style={{ color: "hotpink"}} href='https://sjoochland.se/' target="_blank" title="Opens in a new window">www.sjoochland.se/</a>
+                 Grattis! Wow! Du är nu med i utlottningen till att vinna en trerätters meny för 4 personer på Westers sommarkrog Sjö & Land. Mitt på strandvägens kaj! Vi hör av oss på mail om du vinner, så länge kan du läsa mer om det och våra andra tjänster för Catering och Event på: <a style={{ color: "hotpink"}} href={westers_url} target="_blank" title="Opens in a new window">www.westers.se</a> och på restaurangen: <a style={{ color: "hotpink"}} href='https://sjoochland.se/' target="_blank" title="Opens in a new window">www.sjoochland.se</a>
                 <Confetti
                     width={window.innerWidth}
                     height={window.innerHeight}
@@ -50,7 +46,7 @@ export default class Winning extends React.Component {
         callAPI(this.props.values.business,this.props.values.email,"Grill")
         return (
             <div className="winner-text">
-                Grattis! Du är nu med i utlottningen till att vinna en grillkasse fylld med råvaror för 10 personer. Såser, marinader och råvaror för en helkväll med dina nära och kära. Vi hör av oss på mail om du vinner, så länge kan du läsa mer om det och våra andra tjänster för Catering och Event på: <a style={{ color: "hotpink"}} href='https://www.westers.se/?gclid=CjwKCAjw6dmSBhBkEiwA_W-EoMIZ_n1hUj9x3TPdAkUCPblovF-hdikZhxGPho7RezLj2P_OKW6-WBoC31gQAvD_BwE' target="_blank" title="Opens in a new window">www.westers.se</a>
+                Grattis! Du är nu med i utlottningen till att vinna en grillkasse fylld med råvaror för 10 personer. Såser, marinader och råvaror för en helkväll med dina nära och kära. Vi hör av oss på mail om du vinner, så länge kan du läsa mer om det och våra andra tjänster för Catering och Event på: <a style={{ color: "hotpink"}} href={westers_url} target="_blank" title="Opens in a new window">www.westers.se</a>
                 <Confetti
                     width={window.innerWidth}
                     height={window.innerHeight}
